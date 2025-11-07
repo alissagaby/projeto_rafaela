135d562336b192fd8eb540d6cc8ce5b39d75b353
alissagaby <alissagabriele756@gmail.com>
2025-11-07 17:17:06 +0000

    adfgn

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyCaW9juUS9nytkFCUT1dydELOou-kziXMw",
+    authDomain: "projeto-rafaela-97742.firebaseapp.com",
+    projectId: "projeto-rafaela-97742",
+    storageBucket: "projeto-rafaela-97742.firebasestorage.app",
+    messagingSenderId: "520939530475",
+    appId: "1:520939530475:web:8ce3ecc9fbbf3bcbc1b6ae",
+    measurementId: "G-N37RHNP9MP"
+  };
+
+firebase.initializeApp(firebaseConfig); 
+  
